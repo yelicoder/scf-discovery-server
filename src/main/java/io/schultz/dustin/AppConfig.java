116583083fac0925b0aeabccba17bf5e9cb084f5
yelicoder <yelicoder>
2022-02-09 15:57:48 -0500

    Added code for AWS support

---
+++ src/main/java/io/schultz/dustin/AppConfig.java
@@ -0,0 +1,22 @@
+package io.schultz.dustin;
+
+import org.springframework.cloud.commons.util.InetUtilsProperties;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.netflix.appinfo.AmazonInfo;
+
+// This class is used if the server is deployed in AWS
+@Configuration
+public class AppConfig {
+	
+	@Bean
+	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtilsProperties properties) {
+		
+		EurekaInstanceConfigBean bean = new EurekaInstanceConfigBean(new InetUtils(properties));
+		AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+		bean.setDataCenterInfo(info);
+		return bean;
+	}	
+}
